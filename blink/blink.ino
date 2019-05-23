fdbcfe0f9898455fabf65ff1ba1cdd4b84625786
Chris <chrismjboyle@gmail.com>
2019-05-23 12:00:56 +0100

    Added formatting to serial output

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,15 +1,12 @@
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
 }
 
-// the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(100);
   digitalWrite(LED_BUILTIN, LOW);
   delay(100);
-  Serial.print("hi");
+  Serial.print("\033[1mhi\033[0mhi");
 }
