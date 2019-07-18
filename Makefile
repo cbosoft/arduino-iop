0e2f538ef2b5cc124d1aaa63e25d943c0cb66d53
Chris <chrismjboyle@gmail.com>
2019-07-18 12:35:40 +0100

    Moved iop to library, added ref implementation "serlist"

--- Makefile
+++ Makefile
@@ -1,6 +1,24 @@
-CC = gcc
-CFLAGS = -Wall -Wextra
-OBJ = src/main.o
+CC 			= gcc
+CFLAGS 	= -Wall -Wextra
+SO			=	libardiop.so
+IOP 		= src/ardiop.o
+HDR			=	src/ardiop.h
 
-iop: $(OBJ)
-	$(CC) $(CFLAGS) $(OBJ) -o $@
+.DEFAULT: ardiop
+
+src/%.o: src/%.c
+	$(CC) $(CFLAGS) $< -c -fPIC -o $@
+
+libardiop.so: $(IOP)
+	$(CC) $(CFLAGS) $(IOP) -o $(SO) -shared
+
+install: libardiop.so src/ardiop.h
+	cp $(SO) /usr/lib/.
+	cp $(HDR) /usr/include/.
+
+uninstall:
+	rm /usr/lib/$(SO)
+	rm /usr/include/$(HDR)
+
+serlist: src/main.c
+	$(CC) $(CFLAGS) $< -o serlist -lardiop
--- src/main.c
+++ src/main.c
@@ -1,74 +1,40 @@
 #define _DEFAULT_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
 #include <errno.h>
- 
-int serialport_read_until(int fd, char* buf, char until, int buf_max, int timeout) {
-  char b[1];  // read expects an array, so we give it a 1-byte array
-  int i=0;
-  do {
-    int n = read(fd, b, 1);  // read a char at a time
-    if(n == -1) return -1;    // couldn't read
-    if(n == 0) {
-      usleep(1000);  // wait 1 msec try again
-      timeout--;
-      if(timeout == 0) return -2;
-        continue;
-    }
-    buf[i] = b[0];
-    i++;
-  } while( b[0] != until && i < buf_max && timeout>0 );
 
-  buf[i] = 0;  // null terminate the string
-  return 0;
-}
+#include <ardiop.h>
  
+#define BUFMAX 256
+
+
+
+static int interrupted = 0;
+
+
+
+void sighand(int sig)
+{
+  interrupted = 1;
+}
+
+
+
 int main(int argc, char *argv[])
 {
-  int fd;
-  const char *portname = "/dev/ttyACM0";
-  char buf[256];
- 
-  fd = open(portname, O_RDWR | O_NOCTTY);
-  if (fd < 0) {
-    fprintf(stderr, "(%d) %s\n", errno, strerror(errno));
-    exit(1);
-  }
+  const char *serial_port_path = "/dev/ttyACM0";
+  char buf[BUFMAX];
 
-  struct termios toptions;
-  tcgetattr(fd, &toptions);
-  cfsetispeed(&toptions, B9600);
-  cfsetospeed(&toptions, B9600);
-
-  toptions.c_cflag &= ~PARENB;
-  toptions.c_cflag &= ~CSTOPB;
-  toptions.c_cflag &= ~CSIZE;
-  toptions.c_cflag |= CS8;
-  toptions.c_cflag &= ~CRTSCTS;
-  toptions.c_cflag |= CREAD | CLOCAL;
-  toptions.c_iflag &= ~(IXON | IXOFF | IXANY);
-  toptions.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-  toptions.c_oflag &= ~OPOST;
-
-  toptions.c_cc[VMIN] = 1;
-  toptions.c_cc[VTIME] = 0;
-
-  tcsetattr(fd, TCSANOW, &toptions);
-
-  usleep(1000000);
-  tcflush(fd, TCIFLUSH);
-  
+  int fd = ard_openserial(serial_port_path);
 
   do {
-    serialport_read_until(fd, buf, '\n', 256, 1000);
-    fprintf(stdout, "%s", buf);
-    fflush(stdout);
-  } while (1);
+    ard_readserial_line(fd, buf, BUFMAX, 1000);
+    fprintf(stderr, "%s", buf);
+  } while (!interrupted);
 
   return 0;
 }
